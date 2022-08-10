6f9267d20e8f95a58b44eb3d8e5eec89fc0cebaa
gl4tsch <florian.hoeschl@googlemail.com>
2022-08-10 02:50:50 +0200

    cell neighbours prep

--- Assets/Scripts/MapGenerator.cs
+++ Assets/Scripts/MapGenerator.cs
@@ -70,17 +70,18 @@ public class MapGenerator : MonoBehaviour
         DrawVoronoi();
 
         var delaunatorCells = delaunator.GetVoronoiCells();
+        var delaunatorCellArray = delaunatorCells.ToArray();
 
-        //foreach(var cell in delaunatorCells)
-        //{
-        //    var pos = Delaunator.GetCentroid(cell.Points);
-        //    Instantiate(delaunayPointPrefab, new Vector3((float)pos.X, 0, (float)pos.Y), Quaternion.identity);
+        foreach (var cell in delaunatorCells)
+        {
+            //var center = Delaunator.GetCentroid(cell.Points);
+            var center = blueNoisePoints[cell.Index];
 
-        //    foreach(var point in cell.Points)
-        //    {
-        //        Instantiate(voronoiPointPrefab, new Vector3((float)point.X, 0, (float)point.Y), Quaternion.identity);
-        //    }
-        //}
+            foreach (var point in cell.Points)
+            {
+                
+            }
+        }
 
         var camHeight = mapSize * 0.5f / Mathf.Tan(Camera.main.fieldOfView * 0.5f * Mathf.Deg2Rad);
         Camera.main.transform.position = new Vector3(0, camHeight * 1.1f, 0);
@@ -98,12 +99,12 @@ public class MapGenerator : MonoBehaviour
 
         delaunator.ForEachTriangleEdge(edge =>
         {
-            if (drawDelaunayPoints)
+            if (drawDelaunayEdges)
             {
                 CreateLine(delaunayVisualsContainer, $"TriangleEdge - {edge.Index}", new Vector3[] { new Vector3((float)edge.P.X, 0, (float)edge.P.Y), new Vector3((float)edge.Q.X, 0, (float)edge.Q.Y) }, delaunayEdgeColor, 0.1f, 0);
             }
 
-            if (drawDelaunayEdges)
+            if (drawDelaunayPoints)
             {
                 var pointGameObject = Instantiate(delaunayPointPrefab, new Vector3((float)edge.P.X, 0, (float)edge.P.Y), Quaternion.identity, delaunayVisualsContainer);
             }
